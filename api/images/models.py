6e6362b5b3c11553cb97bde257ad1ef5c40bca4e
Pawel <pawlo0107@gmail.com>
2023-01-23 22:56:04 +0100

    Add Image model with migration

---
+++ api/images/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+def update_name(instance, filename):
+    _, extension = filename.split('.')
+    return f"{instance.title}.{extension}"
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=128, verbose_name=_("title"), unique=True)
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    url = models.ImageField(upload_to=update_name)
+    created_at = models.DateTimeField(default=timezone.now, verbose_name=_("created at"))
+
+    class Meta:
+        verbose_name = _("image")
+        verbose_name_plural = _("images")
+
+    def save(self, *args, **kwargs):
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
